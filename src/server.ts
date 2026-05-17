a1056576c62f950e7992a075b15b764a560c9a38
suroj <surojcodes@gmail.com>
2026-05-17 09:50:29 -0700

    add create user tool

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,59 @@
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
-import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
+import users from './data/users.json' with { type: 'json' };
+import fs from 'node:fs/promises';
 
 const server = new McpServer({
   name: 'My MCP Server',
   description: 'A simple MCP server example',
   version: '1.0.0',
-})
+});
 
-async function main(){
-  const transport = new StdioServerTransport()
-  await server.connect(transport)
+server.registerTool("create-user", {
+  title: "Create User",
+  description: "Creates a new user with the given name and email.",
+  inputSchema: {
+    name: z.string(),
+    email: z.string(),
+    address: z.string(),
+    phone: z.string(),
+  },
+  annotations: {
+    title: "Create User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true
+  }
+}, async (params) => {
+  try {
+    const id = await createUser(params);
+    return {
+      content: [
+        { type: "text", text: `User created successfully with ID: ${id}` }
+      ]
+    };
+  } catch (e) {
+    console.error("Error creating user:", e);
+    return {
+      content: [
+        { type: "text", text: "An error occurred while creating the user." }
+      ]
+    };
+  }
+});
+
+
+async function createUser(user: { name: string; email: string; address: string; phone: string; }) {
+  const id = users.length + 1;
+  users.push({ id, ...user });
+  await fs.writeFile('./src/data/users.json', JSON.stringify(users, null, 2));
+  return id;
 }
-main()
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+main();
